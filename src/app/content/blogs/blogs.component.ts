c10cbc2c473899b2b0ce3046459deb18cebdfa80
Arpita.s <arpita.sahoo637174@gail.com>
2024-01-24 00:00:11 +0530

    medium api implemented

--- src/app/content/blogs/blogs.component.ts
+++ src/app/content/blogs/blogs.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit, inject } from '@angular/core';
 
 @Component({
   selector: 'app-blogs',
   templateUrl: './blogs.component.html',
   styleUrls: ['./blogs.component.css']
 })
-export class BlogsComponent {
-
+export class BlogsComponent implements OnInit{
+  ngOnInit(): void {
+    this.FetchDataFromMedium();
+   }
+  httpClient = inject(HttpClient);
+  blogData:any;
+  FetchDataFromMedium = () =>{
+    this.httpClient.get('https://v1.nocodeapi.com/arpita/medium/lDpkExdsptxWGvqO').subscribe((data:any)=>{
+      // console.log(data);
+      this.blogData = data;
+      // console.log(this.blogData);
+      // console.log(this.blogData);
+      
+    })
+  }
 }
--- src/app/content/projects/project-content/datetime-formatter/datetime-formatter.component.ts
+++ src/app/content/projects/project-content/datetime-formatter/datetime-formatter.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./datetime-formatter.component.css']
 })
 export class DatetimeFormatterComponent {
-
+  constructor(){}
+   DateTimeFormatter = [
+    {
+      projectname:'ClockFormatter',
+      text:'I built a simple todo Web app for easy task management. It lets you add tasks, check them off, User can tag them as their types, I have used Localstorage for seamless user experience,The design is clean, and it works smoothly on any device.',
+      skills:['Angular','TypeScript','Bootstrap','AngularMaterial'],
+      duration:'2 days',
+      projectlink:'https://github.com/Arpita-p-sahoo/clock-formatter'
+    }
+   ]
 }
+// I built a simple todo Web app for easy task management. It lets you add tasks, check them off,
+//       User can tag them as their types, I have used Localstorage for seamless user experience,
+//        The design is clean, and it works smoothly on any device.
